c9cd3a5650341e5417c1d6998a0d8c2ba7203cc9
Dylan <dylanlancekemboi@gmail.com>
2023-10-10 09:22:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    // Convert the search name and driver names to lowercase for case-insensitive comparison
+    const lowerCaseName = name.toLowerCase();
+  
+    // Use the filter method to find matching drivers
+    const matchingDrivers = drivers.filter(function(driver) {
+        const lowerCaseDriverName = driver.toLowerCase();
+
+        return lowerCaseDriverName === lowerCaseName;
+    });
+  
+    return matchingDrivers;
+}
+
+
+function fuzzyMatch(drivers, query) {
+    const lowerCaseQuery = query.toLowerCase();
+    return drivers.filter(function(driver) {
+        const lowerCaseDriverName = driver.toLowerCase();
+        return lowerCaseDriverName.startsWith(lowerCaseQuery);
+    });
+}
+  
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+      return driver.name === name;
+    });
+}
+  
